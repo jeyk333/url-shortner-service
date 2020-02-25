9b00d621b0f2452b6d103b6469b92fe9ea28eb0b
vome_developer <jeyanth.raju@purviewservices.com>
2020-02-25 23:28:37 +0530

    DB issue resolved

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = require("./keys");
 const connectDB = async () => {
   try {
     await mongoose.connect(db.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("mongoDB connected");
+    console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
